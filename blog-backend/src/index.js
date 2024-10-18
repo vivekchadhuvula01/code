bc75beef87a95bd4dd693f9865ac7829cdeb2db5
vivekchadhuvula01 <vivekchadhuvula@gmail.com>
2024-10-18 15:58:03 +0530

    examine of routes using postman

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 
 const app = express();
-
+app.use(express.json());
 app.listen(4567, () => {
     console.log(`server is running on port http://localhost:${4567}`);
 })
 
 app.get('/', (req, res) => { 
     res.send("hello")
+})
+app.post('/article', (req, res) => { 
+    console.log(req.body)
+    res.send(`here is your ${req.body.article}`)
 })
